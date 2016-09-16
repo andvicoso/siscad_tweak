e54b012450ad8b10f48bb1102e00a23d3c191f10
andvicoso <andvicoso@gmail.com>
2016-09-16 11:41:16 -0400

    highlightLinks added to utils

--- export_stds_siscad_tweak.user.js
+++ export_stds_siscad_tweak.user.js
@@ -6,58 +6,49 @@
 // @grant       none
 // @include https://siscad.ufms.br/titan.php?toSection=3&toAction=view*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require https://raw.githubusercontent.com/andvicoso/siscad_tweak/master/siscad_tweak_utils.js
 // ==/UserScript==
-function downloadFile(filename, url) {
-  var a = document.createElement('a');
-  a.href = url;
-  a.download = filename;
-  document.body.appendChild(a);
-  a.click();
-  setTimeout(function () {
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
-  }, 100);
-}
-function makeCSVFile(text) {
-  var data = new Blob([text], {
-    type: 'text/csv'
-  });
-  // returns a URL you can use as a href
-  return window.URL.createObjectURL(data);
-}
-function getLine(obj) {
-  return obj.parent().parent();
-}
-function getName(line) {
-  return line.children(':nth-child(2)').text();
-}
-function getRGA(line) {
-  return line.children(':nth-child(1)').text().trim();
-}
-function getAbsence(line) {
-  return line.children(':nth-child(5)').text().trim();
-}
-function getPresence(line) {
-  return line.children(':nth-child(6)').text().trim();
-}
-$('.logoApplication').append('<div id=\'twk\' style=\'top:50;left:230;position:absolute;background-color:red;font-size:large;color:black\'><b>TWEAKED!</b></div>');
-$('#diaLetivoId').parent().last().after('<div id="csv" style="border: 1px solid black;"><input id="csv_btn" type="button" value="to CSV"><input type="checkbox" id="nome_csv" checked="checked">Nome<input type="checkbox" id="rga_csv" checked="checked">RGA<input type="checkbox" id="absent_csv" checked="checked">Faltas<input type="checkbox" id="presence_csv" checked="checked">Presenças</div>');
+appendLogo();
+var failed = 0;
+var warning = 0;
+var all = $('tr.cabecalho').parent().parent().find('tr');
+var total = all.length - 1;
+all.each(function (index) {
+  if (index == 0) return;
+  var line = $(this);
+  var value = getValue(getData(line, 4));
+  highlightLinks(line);
+  if (isFailed(value)) {
+    failed++;
+    //change color for the students with more than 26% of faults
+    line.css('background-color', '#ff7d66'); //old tomato
+  } 
+  else if (isWarning(value)) {
+    warning++;
+    //change color for the students with more than 19% of faults
+    line.css('background-color', 'khaki');
+  }
+});
+//summary table
+appendSummaryTable(warning, failed, total);
+//csv export inputs
+var inputs = '';
+$('td.cabecalho').each(function (index) {
+  var text = $(this).text().trim();
+  inputs += '<input type="checkbox" id="col' + index + '_csv" checked="checked">' + text;
+});
+$('#diaLetivoId').parent().last().after('<div id="csv" style="border: 1px solid black;"><input id="csv_btn" type="button" value="to CSV">' + inputs + '</div>');
 $('#csv_btn').click(function () {
   var csv = '';
-  $('tr.cabecalho').parent().each(function (index) {
-    var line = getLine($(this));
-    if ($('#rga_csv').prop('checked')) {
-      csv += getRGA(line) + ';';
-    }
-    if ($('#nome_csv').prop('checked')) {
-      csv += getName(line) + ';\n';
-    }
-    if ($('#absence_csv').prop('checked')) {
-      csv += getAbsence(line) + ';\n';
-    }
-    if ($('#presence_csv').prop('checked')) {
-      csv += getPresence(line) + ';\n';
-    }
+  all.each(function (index) {
+    var line = $(this);
+    $('td.cabecalho').each(function (index2) {
+      var input_id = 'col' + index2 + '_csv';
+      if ($('#' + input_id).prop('checked')) {
+        csv += getData(line, index2) + ';';
+      }
+    });
+    csv += '\n';
   });
   //csv para download
   downloadFile('csv_alunos.csv', makeCSVFile(csv));
--- siscad_tweak_utils.js
+++ siscad_tweak_utils.js
@@ -17,7 +17,7 @@ function makeCSVFile(text) {
   return window.URL.createObjectURL(data);
 }
 function getValue(x) {
-   var posabre = x.indexOf('(');
+  var posabre = x.indexOf('(');
   var pospercent = x.indexOf('%');
   var percent = x.substring(posabre + 1, pospercent);
   return parseFloat(percent.replace(',', '.'));
@@ -34,6 +34,23 @@ function isFailed(value) {
 function isWarning(value) {
   return value > 19;
 }
+function highlightLinks(line){
+  var links = line.find('a');
+  //make the name bold on hover
+  line.hover(function () {
+    links.css('font-weight', 'Bold');
+  }, function () {
+    links.css('font-weight', '');
+  }
+  );
+}
 function appendLogo(){
 	$('.logoApplication').append('<div id=\'twk\' style=\'top:50;left:230;position:absolute;background-color:red;font-size:large;color:black\'><b>TWEAKED!</b></div>');
+}
+function appendSummaryTable(warning, failed, total){
+  var active = total - failed - warning;
+  var activep = percent(active, total);
+  var warningp = percent(warning, total);
+  var failedp = percent(failed, total);
+  $('.caixaAzul').last().after('<table style="border-collapse:collapse;border-spacing:0;margin-left:auto;margin-right: auto;"><tr><td style="font-weight:bold;border-style:solid;border-width:1px;padding:5px;text-align:center;">Situação<br></td><td style="font-weight:bold;border-style:solid;border-width:1px;padding:5px;text-align:center;">Quantidade (%)<br></td></tr><tr><td style="border-style:solid;border-width:1px;padding:5px;background-color:#ff7d66;color:#ffffff;">Reprovados por falta</td><td style="border-style:solid;border-width:1px;padding:5px;background-color:#ff7d66;color:#ffffff;text-align:center;">' + failed + ' (' + failedp + '%)' + '</td></tr><tr><td style="border-style:solid;border-width:1px;padding:5px;background-color:khaki;">Atenção</td><td style="border-style:solid;border-width:1px;padding:5px;background-color:khaki;text-align:center;">' + warning + ' (' + warningp + '%)' + '</td></tr><tr><td style="border-style:solid;border-width:1px;padding:5px;background-color:#9aff99;">Ativos<br></td><td style="border-style:solid;border-width:1px;padding:5px;background-color:#9aff99;text-align:center;">' + active + ' (' + activep + '%)' + '</td></tr><tr><td style="border-style:solid;border-width:1px;padding:5px;font-weight:bold;">Total</td><td style="border-style:solid;border-width:1px;padding:5px;text-align:center;font-weight:bold;">' + total + '</td></tr></table>');
 }
