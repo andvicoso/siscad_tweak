85f1a6f28591197de5767423f1c1d740a8807b89
andvicoso <andvicoso@gmail.com>
2018-07-31 15:23:13 -0400

    fixed auto login

--- general_siscad_tweak.user.js
+++ general_siscad_tweak.user.js
@@ -3,20 +3,24 @@
 // @author      andvicoso
 // @namespace   andvicoso_siscad_tweak
 // @description Siscad initial auto-login
-// @version     1.6
+// @version     1.7
 // @grant       none
 // @icon        https://siscad.ufms.br/favicon.ico
 // @downloadURL https://github.com/andvicoso/siscad_tweak/raw/master/general_siscad_tweak.user.js
 // @include     https://siscad.ufms.br/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js
 // @require     https://raw.githubusercontent.com/andvicoso/siscad_tweak/master/siscad_tweak_utils.js
+// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant    GM_addStyle
 // ==/UserScript==
-
+/*- The @grant directive is needed to work around a major design
+    change introduced in GM 1.0.
+    It restores the sandbox.
+*/
 this.$ = this.jQuery = jQuery.noConflict(true);
+waitForKeyElements ("#formLogin", actionFunction);
 
-(function() {
-    'use strict';
-
+function actionFunction () {
     if ($('#formLogin').length !== 0) {
         if ($('#password').val() != '' && $('#login').val() != '') {
             document.getElementById('formLogin').submit();
@@ -37,4 +41,4 @@ this.$ = this.jQuery = jQuery.noConflict(true);
         "border-right":" 1px solid #333333",
         "border-bottom":" 1px solid #333333",
         "border-left":" 1px solid #CCCCCC"});
-})();
+}
